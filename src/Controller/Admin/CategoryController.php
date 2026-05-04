77e3cde5fdd088bf4e7f70fb2a377543cd8734d0
Salma Maqal <maqalsalma81@gmail.com>
2026-05-04 15:13:39 +0200

    modification category CRUD

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -9,14 +9,19 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Category;
 use App\Form\CategoryFormType;
+use App\Repository\CategoryRepository;
 
 final class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
+
+        $categories = $categoryRepository->findAll();
+
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
+            'categories' => $categories,
         ]);
     }
 
@@ -26,12 +31,46 @@ final class CategoryController extends AbstractController
         $category = new Category();
         $form = $this->createForm(CategoryFormType::class, $category);  
         $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+        
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_category');
 
-        return $this->render('category/new.html.twig', [
-    'form' => $form->createView(),
             
-        ]);
-
-
+        }
+        return $this->render('category/new.html.twig', [
+    'form' => $form->createView(), ]);
   }
+
+    #[Route('/category/{id}/update', name: 'app_category_update')]
+    public function update(Request $request, EntityManagerInterface $entityManager, Category $category): Response
+    {
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            return $this->redirectToRoute('app_category');
+
+        }
+
+        return $this->render('category/update.html.twig', [
+            'form' => $form->createView(),
+            'category' => $category, ]);
+    }
+
+#[Route('/category/{id}/delete', name: 'app_category_delete')]
+    public function delete( EntityManagerInterface $entityManager, Category $category): Response
+    {
+        $entityManager->remove($category);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_category');
+        
+    }
+
+
+
+
 }
